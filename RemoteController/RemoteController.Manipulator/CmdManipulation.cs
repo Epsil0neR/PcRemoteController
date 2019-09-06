f2adf7457068cbf76f2bab8dc8d7ed3710b229db
Veniamin Lytkin <vlyt@stormgeo.com>
2019-09-06 09:03:56 +0300

    Added documentation and improved CMD manipulation.

--- RemoteController/RemoteController.Manipulator/CmdManipulation.cs
+++ RemoteController/RemoteController.Manipulator/CmdManipulation.cs
@@ -3,17 +3,36 @@ using System.Diagnostics;
 
 namespace RemoteController.Manipulator
 {
+    /// <summary>
+    /// Manipulation with CMD command.
+    /// </summary>
     public class CmdManipulation : IManipulation
     {
-        private string _command;
-        public CmdManipulation(string name, string cmdArgument)
+        private readonly string _command;
+        private readonly bool _hidden;
+        private readonly bool _wait;
+
+        /// <summary>
+        /// Creates manipulation instance for CMD
+        /// </summary>
+        /// <param name="name">Name of manipulation. Must be unique in list of used.</param>
+        /// <param name="cmdArgument">CMD command to execute</param>
+        /// <param name="hidden">Indicates if CMD window will be hidden or shown.</param>
+        /// <param name="wait">Indicates if CMD command execution should be waited.</param>
+        public CmdManipulation(string name, string cmdArgument, bool hidden = false, bool wait = false)
         {
             Name = name ?? throw new ArgumentNullException(nameof(name));
             _command = cmdArgument ?? throw new ArgumentNullException(nameof(cmdArgument));
+            _hidden = hidden;
+            _wait = wait;
         }
 
+        /// <inheritdoc />
         public string Name { get; }
 
+        /// <summary>
+        /// Executes CMD command.
+        /// </summary>
         public void Execute()
         {
             Process proc = new Process
@@ -22,12 +41,15 @@ namespace RemoteController.Manipulator
                 {
                     FileName = "CMD.exe",
                     Arguments = _command,
-                    WindowStyle = ProcessWindowStyle.Hidden
                 }
             };
-            proc.Start();
-            //proc.WaitForExit();
-        }
 
+            if (_hidden)
+                proc.StartInfo.WindowStyle = ProcessWindowStyle.Hidden;
+
+            proc.Start();
+            if (_wait)
+                proc.WaitForExit();
+        }
     }
 }
--- RemoteController/RemoteController.Manipulator/IManipulation.cs
+++ RemoteController/RemoteController.Manipulator/IManipulation.cs
@@ -2,6 +2,9 @@
 {
     public interface IManipulation
     {
+        /// <summary>
+        /// Name of manipulation. Must be unique in list of used.
+        /// </summary>
         string Name { get; }
     }
 }
