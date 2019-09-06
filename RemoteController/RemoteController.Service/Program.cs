807fc3c316d674b0a8f97bfd678c5cf6e91ce388
Veniamin Lytkin <vlyt@stormgeo.com>
2019-09-06 17:17:30 +0300

    Migrating to windows service logic.

--- RemoteController/RemoteController.Service/Program.cs
+++ RemoteController/RemoteController.Service/Program.cs
@@ -4,6 +4,8 @@ using WindowsInput;
 using WindowsInput.Native;
 using RemoteController.Manipulator;
 using RemoteController.WebSocket;
+using Topshelf;
+using Topshelf.Logging;
 using WebSocketSharp;
 
 namespace RemoteController.Service
@@ -12,60 +14,64 @@ namespace RemoteController.Service
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            var mm = new ManipulatorsManager();
-            var inputSimulator = new InputSimulator();
-
-            mm.SetContext(inputSimulator.Keyboard);
-            mm.SetContext(inputSimulator.Mouse);
-
-
-            mm.Add(new CmdManipulation("cmd.git.stage", "git add -A"));
-            mm.Add(new CmdManipulation("cmd.git.unstage", "git reset"));
-            mm.Add(new CmdManipulation("cmd.git", "git", true));
-            mm.Add(new CmdManipulation("cmd.git.2", "git", true, false, true));
-            mm.Add(new CmdManipulation("cmd.git.3", "git", true, true, true));
-
-            mm.Add(new KeyboardManipulation("Key"));
-            mm.Add(new KeyboardManipulation("Key.Media.Play", VirtualKeyCode.MEDIA_PLAY_PAUSE));
-            mm.Add(new KeyboardManipulation("Key.Media.Next", VirtualKeyCode.MEDIA_NEXT_TRACK));
-            mm.Add(new KeyboardManipulation("Key.Media.Prev", VirtualKeyCode.MEDIA_PREV_TRACK));
-            mm.Add(new KeyboardManipulation("Key.Volume.-", VirtualKeyCode.VOLUME_DOWN));
-            mm.Add(new KeyboardManipulation("Key.Volume.+", VirtualKeyCode.VOLUME_UP));
-            mm.Add(new KeyboardManipulation("Key.Volume.Mute", VirtualKeyCode.VOLUME_MUTE));
-
-            mm.Add(new MouseManipulation("Mouse.Move.X", (simulator, param) =>
+            var rv = HostFactory.Run(x =>
             {
-                if (!int.TryParse(param, out var x))
-                    x = 100;
-                simulator.MoveMouseBy(x, 0);
-            }));
-            mm.Add(new MouseManipulation("Mouse.Move.Y", (simulator, param) =>
-            {
-                if (!int.TryParse(param, out var y))
-                    y = 100;
-                simulator.MoveMouseBy(0, y);
-            }));
 
-            while (true)
-            {
-                Console.Write("Command name: ");
-                var name = Console.ReadLine();
+                x.Service<MyService>(c =>
+                {
+                    x.UseNLog();
+                    c.ConstructUsing(name => new MyService(HostLogger.Current.Get(string.Empty)));
+                    c.WhenStarted((s, host) => s.Start(host));
+                    c.WhenStopped((s, host) => s.Stop(host));
+                });
 
-                if (string.IsNullOrWhiteSpace(name))
-                    break;
+                x.EnableServiceRecovery(rc =>
+                {
+                    // first failure
+                    rc.RestartService(TimeSpan.FromMinutes(1));
 
-                Console.Write("Parameter:    ");
-                var param = Console.ReadLine();
+                    // second failure
+                    rc.RestartService(TimeSpan.FromMinutes(1));
 
-                Console.WriteLine($"Executed: {mm.TryExecute(name, param)}");
-                Console.WriteLine();
-            }
+                    // subsequent failures
+                    rc.RestartService(TimeSpan.FromMinutes(2));
+                });
 
-            RunWebSocketServer();
+                x.SetDisplayName("PC Remote controller");
+                x.SetDescription("Host service for PC remote controller that lets via WebSocket connection manipulate PC where service runs.");
+                x.SetServiceName("Epsiloner.PcRemoteController");
 
-            Console.WriteLine("Ready to exit...");
-            Console.ReadKey(true);
+                x.RunAsLocalSystem();
+                x.StartAutomatically();
+                x.EnableShutdown();
+            });
+
+            var exitCode = (int)Convert.ChangeType(rv, rv.GetTypeCode());
+            Environment.ExitCode = exitCode;
+
+            //Console.WriteLine("Hello World!");
+            //var mm = new ManipulatorsManager();
+
+
+            //while (true)
+            //{
+            //    Console.Write("Command name: ");
+            //    var name = Console.ReadLine();
+
+            //    if (string.IsNullOrWhiteSpace(name))
+            //        break;
+
+            //    Console.Write("Parameter:    ");
+            //    var param = Console.ReadLine();
+
+            //    Console.WriteLine($"Executed: {mm.TryExecute(name, param)}");
+            //    Console.WriteLine();
+            //}
+
+            //RunWebSocketServer();
+
+            //Console.WriteLine("Ready to exit...");
+            //Console.ReadKey(true);
         }
 
         private static void RunWebSocketServer()
@@ -118,6 +124,99 @@ namespace RemoteController.Service
         private static void ServerOnStarted(object sender, EventArgs e)
         {
             Console.WriteLine("WS - started");
+        }
+    }
+
+    public class MyService : ServiceControl
+    {
+        //THIS LINE IS IMPORTANT - this is how we get logger.
+        public LogWriter Logger { get; }
+
+        public ManipulatorsManager Manipulators { get; }
+        public WsServer Server { get; }
+        public WsService Service { get; }
+
+
+        public MyService(LogWriter logger)
+        {
+
+            Logger = logger ?? HostLogger.Get<MyService>();
+            Manipulators = new ManipulatorsManager();
+            Server = new WsServer("localhost", 6431, "/Testing");
+            Service = new WsService(Server);
+
+            Configurator.Configure(Manipulators);
+            Configurator.Configure(Service);
+        }
+
+
+
+        public bool Start(HostControl hostControl)
+        {
+            Configurator.SetContexts(Manipulators);
+            Server.StartServer();
+            return true;
+        }
+
+        public bool Stop(HostControl hostControl)
+        {
+            Server.StopServer();
+            Configurator.ClearContexts(Manipulators);
+            return true;
+        }
+    }
+
+    internal static class Configurator
+    {
+        public static void Configure(ManipulatorsManager manipulatorsManager)
+        {
+            SetContexts(manipulatorsManager);
+
+            manipulatorsManager.Add(new CmdManipulation("cmd.git.stage", "git add -A"));
+            manipulatorsManager.Add(new CmdManipulation("cmd.git.unstage", "git reset"));
+            manipulatorsManager.Add(new CmdManipulation("cmd.git", "git", true));
+            manipulatorsManager.Add(new CmdManipulation("cmd.git.2", "git", true, false, true));
+            manipulatorsManager.Add(new CmdManipulation("cmd.git.3", "git", true, true, true));
+
+            manipulatorsManager.Add(new KeyboardManipulation("Key"));
+            manipulatorsManager.Add(new KeyboardManipulation("Key.Media.Play", VirtualKeyCode.MEDIA_PLAY_PAUSE));
+            manipulatorsManager.Add(new KeyboardManipulation("Key.Media.Next", VirtualKeyCode.MEDIA_NEXT_TRACK));
+            manipulatorsManager.Add(new KeyboardManipulation("Key.Media.Prev", VirtualKeyCode.MEDIA_PREV_TRACK));
+            manipulatorsManager.Add(new KeyboardManipulation("Key.Volume.-", VirtualKeyCode.VOLUME_DOWN));
+            manipulatorsManager.Add(new KeyboardManipulation("Key.Volume.+", VirtualKeyCode.VOLUME_UP));
+            manipulatorsManager.Add(new KeyboardManipulation("Key.Volume.Mute", VirtualKeyCode.VOLUME_MUTE));
+
+            manipulatorsManager.Add(new MouseManipulation("Mouse.Move.X", (simulator, param) =>
+            {
+                if (!int.TryParse(param, out var x))
+                    x = 100;
+                simulator.MoveMouseBy(x, 0);
+            }));
+            manipulatorsManager.Add(new MouseManipulation("Mouse.Move.Y", (simulator, param) =>
+            {
+                if (!int.TryParse(param, out var y))
+                    y = 100;
+                simulator.MoveMouseBy(0, y);
+            }));
+
+        }
+
+        public static void SetContexts(ManipulatorsManager manipulatorsManager)
+        {
+            var inputSimulator = new InputSimulator();
+            manipulatorsManager.SetContext(inputSimulator.Keyboard);
+            manipulatorsManager.SetContext(inputSimulator.Mouse);
+        }
+
+        public static void ClearContexts(ManipulatorsManager manipulators)
+        {
+            manipulators.SetContext<IKeyboardSimulator>(null);
+            manipulators.SetContext<IMouseSimulator>(null);
+        }
+
+        public static void Configure(WsService service)
+        {
+            throw new NotImplementedException();
         }
     }
 }
