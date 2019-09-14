f71e217a9799e28f70c5b5fdd56d132c4d591a32
Veniamin Lytkin <vlyt@stormgeo.com>
2019-09-14 16:25:51 +0300

    Created base informer with empty sound informer

---
+++ RemoteController/RemoteController.Informer/BaseInformer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace RemoteController.Informer
+{
+    public abstract class BaseInformer
+    {
+        /// <summary>
+        /// Raise when some data in informer has been changed.
+        /// </summary>
+        public event EventHandler Changed;
+
+        /// <summary>
+        /// Raises <see cref="Changed"/> event.
+        /// </summary>
+        protected void RaiseChanged()
+        {
+            Changed?.Invoke(this, EventArgs.Empty);
+        }
+
+        /// <summary>
+        /// Checks for changes in system.
+        /// NOTE: Not all OS components notifies about changes.
+        /// </summary>
+        public abstract void CheckForChanges();
+    }
+}
---
+++ RemoteController/RemoteController.Informer/VolumeInformer.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace RemoteController.Informer
+{
+    /// <summary>
+    /// Sound related data informer.
+    /// </summary>
+    public class SoundInformer : BaseInformer
+    {
+        /// <summary>
+        /// Volume 0-100
+        /// </summary>
+        public int Volume { get; private set; }
+
+        /// <summary>
+        /// Current device used for sound output.
+        /// </summary>
+        public object OutputDevice { get; private set; }
+
+        /// <summary>
+        /// Current device used for sound input.
+        /// </summary>
+        public object InputDevice { get; private set; }
+
+        /// <summary>
+        /// List of enabled sound output devices.
+        /// </summary>
+        public object OutputDeviceList { get; private set; }
+
+        /// <summary>
+        /// List of enabled sound input devices.
+        /// </summary>
+        public object InputDeviceList { get; private set; }
+
+        public override void CheckForChanges()
+        {
+            throw new System.NotImplementedException();
+        }
+    }
+}
