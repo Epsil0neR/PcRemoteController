255d5239916ed744fd2fe65b39af443e5955a08a
Veniamin Lytkin <vlyt@stormgeo.com>
2019-09-16 02:14:57 +0300

    Small refactoring

--- RemoteController/RemoteController.Service/Web/Scripts/files.js
+++ RemoteController/RemoteController.Service/Web/Scripts/files.js
@@ -1,42 +1,45 @@
 ﻿window.addEventListener("load", () => {
-    let key = 'history-FileSystem.List';
+    const cmdBase = 'FileSystem.';
+    const cmdList = cmdBase + 'List';
+    const cmdExec = cmdBase + 'Exec';
+    let key = 'history-' + cmdList;
     var btnTest = document.getElementById('btnTest');
     var divPaths = document.getElementById('paths');
     var divFolders = document.getElementById('folders');
     var divFiles = document.getElementById('files');
 
     var client = window.client;
-    client.AddHandler('FileSystem.List',
-        (msg) => {
-            divPaths.innerHTML = '';
-            divFiles.innerHTML = '';
-            divFolders.innerHTML = '';
+    client.AddHandler(cmdList, (msg) => {
+        // Clear previously set containers.
+        divPaths.innerHTML = '';
+        divFiles.innerHTML = '';
+        divFolders.innerHTML = '';
 
-            if (msg.Data instanceof Object) {
-                var path = msg.Data.path;
-                createPath('', '..');
-                var root = '';
+        if (msg.Data instanceof Object) {
+            var path = msg.Data.path;
+            createPath('', '..');
+            var root = '';
 
-                if (path instanceof Array)
-                    root = path.join('\\') + '\\';
+            if (path instanceof Array)
+                root = path.join('\\') + '\\';
 
-                localStorage[key] = path instanceof Array ? path.join('\\') : '';
+            localStorage[key] = path instanceof Array ? path.join('\\') : '';
 
-                if (path instanceof Array) {
-                    path.forEach((x, i) => {
-                        var p = path.slice(0, i + 1);
-                        var url = p.join('\\');
-                        createPath(url, x);
-                    });
-                }
-                if (msg.Data.folders instanceof Array) {
-                    msg.Data.folders.forEach(x => createAction(root + x, x, 'List', divFolders));
-                }
-                if (msg.Data.files instanceof Array) {
-                    msg.Data.files.forEach(x => createAction(root + x, x, 'Exec', divFiles));
-                }
+            if (path instanceof Array) {
+                path.forEach((x, i) => {
+                    var p = path.slice(0, i + 1);
+                    var url = p.join('\\');
+                    createPath(url, x);
+                });
             }
-        });
+            if (msg.Data.folders instanceof Array) {
+                msg.Data.folders.forEach(x => createAction(root + x, x, cmdList, divFolders));
+            }
+            if (msg.Data.files instanceof Array) {
+                msg.Data.files.forEach(x => createAction(root + x, x, cmdExec, divFiles));
+            }
+        }
+    });
 
     var orig = client.onConnectionStatusChange;
     client.onConnectionStatusChange = onConnChange;
@@ -45,29 +48,25 @@
         if (orig instanceof Function)
             orig(status);
 
-        if (client.readyState == WebSocket.OPEN) {
-            message('FileSystem.List', localStorage[key]);
+        if (client.readyState === WebSocket.OPEN) {
+            message(cmdList, localStorage[key]);
         }
     }
 
     function createPath(url, text) {
         var a = document.createElement('a');
         a.text = text;
-        a.addEventListener('click', () => message('FileSystem.List', url));
+        a.addEventListener('click', () => message(cmdList, url));
         divPaths.appendChild(a);
     }
     function createAction(url, text, action, el) {
         var a = document.createElement('a');
         a.text = text;
-        a.addEventListener('click', () => message('FileSystem.' + action, url));
+        a.addEventListener('click', () => message(action, url));
         el.appendChild(a);
     }
 
-    btnTest.addEventListener('click', btnTestOnClick);
-
-    function btnTestOnClick(parameters) {
-        message('FileSystem.List', localStorage[key]);
-    }
+    btnTest.addEventListener('click', () => onConnChange(client.readyState));
 
     onConnChange(client.readyState);
 }, false);
