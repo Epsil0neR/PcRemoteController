1608490fc251377e490501af42309f2d2b1552f1
Epsil0neR <epsil0neris@gmail.com>
2019-10-13 21:54:41 +0300

    Added instant reconnection when page is focus and websocket is not conencted.

--- RemoteController/RemoteController.Client/ClientApp/src/app/app.component.ts
+++ RemoteController/RemoteController.Client/ClientApp/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { WebSocketService } from './services/web-socket.service';
 
 @Component({
   selector: 'app-root',
@@ -6,4 +7,13 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'app';
+
+  constructor(private service: WebSocketService) {
+    window.addEventListener('focus', e => {
+      if (this.service.isConnected.getValue())
+        return;
+
+      this.service.open();
+    });
+  }
 }
