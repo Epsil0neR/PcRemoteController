f10e0f995f27cbd559a4766492ce81472049420f
Epsil0neR <epsil0neris@gmail.com>
2019-10-21 23:08:13 +0300

    Updated key editor.

--- RemoteController/RemoteController.Client/ClientApp/src/key/key-editor-component/key-editor.component.ts
+++ RemoteController/RemoteController.Client/ClientApp/src/key/key-editor-component/key-editor.component.ts
@@ -1,21 +1,27 @@
 import { Component, OnInit } from '@angular/core';
 import { IControlEditor, IControl } from 'src/core';
+import { IKeyControl } from '../Models/IKeyControl';
 
 @Component({
   selector: 'rc-key-editor',
   templateUrl: './key-editor.component.html',
   styleUrls: ['./key-editor.component.css']
 })
-export class KeyEditorComponent implements IControlEditor, OnInit {
+export class KeyEditorComponent
+  implements IControlEditor, OnInit {
+
+  public data: IKeyControl = null;
+
   constructor() { }
 
   ngOnInit() {
   }
 
-  load(data: IControl): boolean {
-    throw new Error('Method not implemented.');
+  load(data: IKeyControl): boolean {
+    this.data = !!data ? data : null;
+    return true;
   }
   save(): IControl {
-    throw new Error('Method not implemented.');
+    return this.data;
   }
 }
