3f214d781ec9f83207ef0039dff2e6200fd88a96
Epsil0neR <epsil0neris@gmail.com>
2019-11-02 22:45:33 +0200

    Added listing all icons

--- RemoteController/RemoteController.Client/ClientApp/src/core/utils/findIcon.ts
+++ RemoteController/RemoteController.Client/ClientApp/src/core/utils/findIcon.ts
@@ -1,6 +1,6 @@
-import { fas } from '@fortawesome/free-solid-svg-icons';
+import { fas, IconDefinition, IconName } from '@fortawesome/free-solid-svg-icons';
 
-export function findIcon(name: string) {
+export function findIcon(name: string | IconName): IconDefinition {
   const pack = fas;
   for (const key in pack) {
     if (!pack.hasOwnProperty(key))
@@ -10,4 +10,20 @@ export function findIcon(name: string) {
     if (icon.iconName === name)
       return icon;
   }
+}
+
+export function allIcons(): IconDefinition[] {
+  const rv: IconDefinition[] = [];
+
+  const pack = fas;
+  for (const key in pack) {
+    if (!pack.hasOwnProperty(key))
+      continue;
+
+    const icon = pack[key];
+    if (typeof icon.iconName === 'string')
+      rv.push(icon);
+  }
+
+  return rv;
 }
