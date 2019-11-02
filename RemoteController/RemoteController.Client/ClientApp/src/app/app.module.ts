682be940852d6ea31764c41f3f038f9dad63e96f
Veniamin Lytkin <vlyt@stormgeo.com>
2019-11-02 16:20:42 +0200

    Created component for control creation

--- RemoteController/RemoteController.Client/ClientApp/src/app/app.module.ts
+++ RemoteController/RemoteController.Client/ClientApp/src/app/app.module.ts
@@ -20,40 +20,42 @@ import { KeyModule } from 'src/key';
 import { VolumeModule } from 'src/volume';
 import { FileSystemModule } from 'src/file-system';
 import { ControlEditorComponent } from './components/control-editor/control-editor.component';
+import { CreateControlComponent } from './components/create-control/create-control.component';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        NavMenuComponent,
-        HomeComponent,
-        CounterComponent,
-        PageComponent,
-        PageEditorComponent,
-        PageCreateComponent,
-        ControlHostDirective,
-        EnumToArrayPipe,
-        ControlEditorComponent,
-    ],
-    imports: [
-        BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
-        FontAwesomeModule,
-        HttpClientModule,
-        FormsModule,
-        RouterModule.forRoot([
-            { path: '', component: HomeComponent, pathMatch: 'full' },
-            { path: 'counter', component: CounterComponent },
-            { path: 'create', component: PageCreateComponent},
-            { path: 'edit/:name', component: PageEditorComponent },
-            { path: 'p/:name', component: PageComponent }
-        ]),
-        CoreModule.forRoot(),
-        KeyModule.forRoot(),
-        VolumeModule.forRoot(),
-        FileSystemModule.forRoot()
-    ],
-    providers: [
-        EnumToArrayPipe,
-    ],
-    bootstrap: [AppComponent],
+  declarations: [
+    AppComponent,
+    NavMenuComponent,
+    HomeComponent,
+    CounterComponent,
+    PageComponent,
+    PageEditorComponent,
+    PageCreateComponent,
+    ControlHostDirective,
+    EnumToArrayPipe,
+    ControlEditorComponent,
+    CreateControlComponent
+  ],
+  imports: [
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    FontAwesomeModule,
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: HomeComponent, pathMatch: 'full' },
+      { path: 'counter', component: CounterComponent },
+      { path: 'create', component: PageCreateComponent },
+      { path: 'edit/:name', component: PageEditorComponent },
+      { path: 'p/:name', component: PageComponent }
+    ]),
+    CoreModule.forRoot(),
+    KeyModule.forRoot(),
+    VolumeModule.forRoot(),
+    FileSystemModule.forRoot()
+  ],
+  providers: [
+    EnumToArrayPipe,
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule { }
---
+++ RemoteController/RemoteController.Client/ClientApp/src/app/components/create-control/create-control.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'rc-create-control',
+  templateUrl: './create-control.component.html',
+  styleUrls: ['./create-control.component.css']
+})
+export class CreateControlComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
