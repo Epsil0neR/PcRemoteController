8785026b9c3e4d48e03f126b471dab084e1ad202
Veniamin Lytkin <vlyt@stormgeo.com>
2019-11-02 16:46:27 +0200

    Added select for control type and improved styles of select

--- RemoteController/RemoteController.Client/ClientApp/src/app/components/create-control/create-control.component.ts
+++ RemoteController/RemoteController.Client/ClientApp/src/app/components/create-control/create-control.component.ts
@@ -7,10 +7,16 @@ import { ControlsService } from 'src/core';
   styleUrls: ['./create-control.component.css']
 })
 export class CreateControlComponent implements OnInit {
+  controlTypes: { title: string; name: string; }[];
+  selectedControlType: string = null;
 
   constructor(public controlsService: ControlsService) { }
 
   ngOnInit() {
+    this.controlTypes = this.controlsService.getAvailable();
   }
 
+  test(data) {
+    console.log('Test:', data);
+  }
 }
