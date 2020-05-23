bb9b97b087ea04e147b0f1acfb2a0b46e6268812
Epsil0neR <epsil0neris@gmail.com>
2020-05-23 13:39:49 +0300

    Handling domain / task sheduler / appdomain exceptions.

---
+++ RemoteController/RemoteController/DomainExceptionHandler.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Threading;
+using NLog;
+
+namespace RemoteController
+{
+    /// <summary>
+    /// Unhandled event registrar.
+    /// Registers and handles events from domain, tasks and dispatchers.
+    /// </summary>
+    public static class DomainExceptionHandler
+    {
+        public static void HandleDomainExceptions()
+        {
+            UnHandleDomainExceptions();
+
+            AppDomain.CurrentDomain.UnhandledException += OnUnhandledDomainException;
+            TaskScheduler.UnobservedTaskException += OnUnobservedTaskException;
+            Dispatcher.CurrentDispatcher.UnhandledException += OnUnhandledDispatcherException;
+        }
+
+        public static void UnHandleDomainExceptions()
+        {
+            AppDomain.CurrentDomain.UnhandledException -= OnUnhandledDomainException;
+            TaskScheduler.UnobservedTaskException -= OnUnobservedTaskException;
+            Dispatcher.CurrentDispatcher.UnhandledException -= OnUnhandledDispatcherException;
+        }
+
+        public static void OnUnhandledDomainException(object sender, UnhandledExceptionEventArgs e)
+        {
+            LogException(e.ExceptionObject as Exception);
+        }
+
+        public static void OnUnobservedTaskException(object sender, UnobservedTaskExceptionEventArgs e)
+        {
+            if (e.Observed)
+                return;
+
+            e.SetObserved();
+            LogAggregatedException(e.Exception);
+        }
+
+        public static void OnUnhandledDispatcherException(object sender, DispatcherUnhandledExceptionEventArgs e)
+        {
+            if (e.Handled)
+                return;
+
+            e.Handled = true;
+            LogException(e.Exception);
+        }
+
+        public static void LogAggregatedException(AggregateException exceptions)
+        {
+            if (exceptions?.InnerExceptions == null)
+                return;
+
+            foreach (var exception in exceptions.InnerExceptions)
+            {
+                LogException(exception);
+            }
+        }
+
+        public static void LogException(Exception exception)
+        {
+            if (exception == null)
+                return;
+
+            Log.Logger.Log(LogLevel.Fatal, exception);
+        }
+
+    }
+}
---
+++ RemoteController/RemoteController/Log.cs
@@ -0,0 +1,25 @@
+﻿using System.IO;
+using System.Reflection;
+using NLog;
+
+namespace RemoteController
+{
+    /// <summary>
+    /// Log based on NLog
+    /// </summary>
+    public static class Log
+    {
+        static Log()
+        {
+            var root = Path.GetDirectoryName(Assembly.GetExecutingAssembly().Location);
+            PathToLogsDirectory = Path.Combine(root, "logs");
+        }
+
+        /// <summary>
+        /// Direct exposure on instantiate ILogger from NLog
+        /// </summary>
+        public static Logger Logger { get; } = LogManager.GetLogger("general");
+
+        public static string PathToLogsDirectory { get; }
+    }
+}
