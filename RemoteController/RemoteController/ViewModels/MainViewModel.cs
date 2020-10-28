b09c7f8daf4ca2f9df3d040ec85d15b60893c469
Epsil0neR <epsil0neris@gmail.com>
2020-10-29 01:05:48 +0200

    Added more content to window.

--- RemoteController/RemoteController/ViewModels/MainViewModel.cs
+++ RemoteController/RemoteController/ViewModels/MainViewModel.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Windows.Media.Animation;
+using NLog;
+using RemoteController.Configs;
 using RemoteController.WebSocket;
-using WebSocketSharp.Server;
 
 namespace RemoteController.ViewModels
 {
@@ -9,13 +11,19 @@ namespace RemoteController.ViewModels
         //public RemoteControllerService Service { get; }
 
         public WsServer WsServer { get; }
+        public ServerConfig ServerConfig { get; }
+        public FileSystemConfig FileSystemConfig { get; }
 
 
-        public MainViewModel(WsServer wsServer)
+        public MainViewModel(
+            WsServer wsServer,
+            ServerConfig serverConfig,
+            FileSystemConfig fileSystemConfig,
+            ILogger logger)
         {
-            WsServer = wsServer ?? throw  new ArgumentNullException(nameof(wsServer));
-            //Service = service;
-            //Service.Start(null);
+            WsServer = wsServer ?? throw new ArgumentNullException(nameof(wsServer));
+            ServerConfig = serverConfig ?? throw new ArgumentNullException(nameof(serverConfig));
+            FileSystemConfig = fileSystemConfig ?? throw new ArgumentNullException(nameof(fileSystemConfig));
         }
     }
 }
