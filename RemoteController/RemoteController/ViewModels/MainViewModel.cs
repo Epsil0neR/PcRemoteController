85a7edd0ba153693289cb8faebdcefe5c6af8812
Epsil0neR <epsil0neris@gmail.com>
2020-10-31 15:56:47 +0200

    [WIP] Added interface with first implementation for pages.

--- RemoteController/RemoteController/ViewModels/MainViewModel.cs
+++ RemoteController/RemoteController/ViewModels/MainViewModel.cs
@@ -1,8 +1,11 @@
 ﻿using System;
+using System.Windows.Input;
 using System.Windows.Media.Animation;
+using Epsiloner.Wpf.ViewModels;
 using NLog;
 using RemoteController.Configs;
 using RemoteController.WebSocket;
+using Unity;
 
 namespace RemoteController.ViewModels
 {
@@ -25,5 +28,54 @@ namespace RemoteController.ViewModels
             ServerConfig = serverConfig ?? throw new ArgumentNullException(nameof(serverConfig));
             FileSystemConfig = fileSystemConfig ?? throw new ArgumentNullException(nameof(fileSystemConfig));
         }
+    }
+    public interface IPageViewModel : IViewModel
+    {
+        /// <summary>
+        /// Page name displayed in UI
+        /// </summary>
+        string Name { get; }
+
+        /// <summary>
+        /// Indicates if page is currently selected.
+        /// </summary>
+        bool IsSelected { get; set; }
+    }
+
+    public abstract class BasePageViewModel : ViewModel, IPageViewModel
+    {
+        private bool _isSelected;
+
+        protected BasePageViewModel(string name)
+        {
+            Name = name;
+        }
+
+        /// <inheritdoc />
+        public string Name { get; }
+
+        /// <inheritdoc />
+        public bool IsSelected
+        {
+            get => _isSelected;
+            set => Set(ref _isSelected, value);
+        }
+    }
+
+    public class OverviewPageViewModel : BasePageViewModel
+    {
+        public OverviewPageViewModel()
+            : base("Overview")
+        {
+        }
+
+        /// <summary>
+        /// TODO: Count of currently connected clients.
+        /// </summary>
+        public int Connections { get; }
+
+        public bool IsServerRunning { get; }
+        public ICommand StopServerCommand { get; }
+        public ICommand StartServerCommand { get; }
     }
 }
