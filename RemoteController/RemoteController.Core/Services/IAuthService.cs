de8587252403eb728a3a6d85a323b9bb9f1bbfb7
Epsil0neR <epsil0neris@gmail.com>
2020-10-31 14:39:41 +0200

    Fixed interface signature.

--- RemoteController/RemoteController.Core/Services/IAuthService.cs
+++ RemoteController/RemoteController.Core/Services/IAuthService.cs
@@ -2,9 +2,9 @@
 {
     public interface IAuthService
     {
-        public bool TryAuthorize(string token);
-        public bool IsAuthorized(string token);
-        public bool Register(string token, string username, string description); //TODO: Add extra fields like Username, Description, Roles
+        bool TryAuthorize(string token);
+        bool IsAuthorized(string token);
+        bool Register(string token, string username, string description); //TODO: Add extra fields like Username, Description, Roles
     }
 
     public interface IRolesService
