db5d57ece9a3421f9291304a651851913c17460c
Epsil0neR <epsil0neris@gmail.com>
2020-11-06 01:40:28 +0200

    Added views and button for pages.

---
+++ RemoteController/RemoteController/Views/PageButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using Epsiloner.Wpf.Attributes;
+using RemoteController.ViewModels.Pages;
+
+namespace RemoteController.Views
+{
+    public class PageButton : Button
+    {
+        static PageButton()
+        {
+            var t = typeof(PageButton);
+            DefaultStyleKeyProperty.OverrideMetadata(t, new FrameworkPropertyMetadata
+            {
+                DefaultValue = t
+            });
+        }
+    }
+
+    [ViewFor(typeof(OverviewPageViewModel))]
+    public class OverviewPageView : Control
+    {
+        static OverviewPageView()
+        {
+            var t = typeof(OverviewPageView);
+            DefaultStyleKeyProperty.OverrideMetadata(t, new FrameworkPropertyMetadata
+            {
+                DefaultValue = t
+            });
+        }
+    }
+}
