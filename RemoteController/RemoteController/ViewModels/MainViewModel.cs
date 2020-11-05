b57effb2aca7187f4c4ae1c3a4f5bd786f9055a4
Epsil0neR <epsil0neris@gmail.com>
2020-11-06 01:44:24 +0200

    Moved old content to page.

--- RemoteController/RemoteController/ViewModels/MainViewModel.cs
+++ RemoteController/RemoteController/ViewModels/MainViewModel.cs
@@ -3,20 +3,12 @@ using System.Linq;
 using System.Windows.Input;
 using Epsiloner.Wpf.ViewModels;
 using GalaSoft.MvvmLight.CommandWpf;
-using NLog;
-using RemoteController.Configs;
-using RemoteController.WebSocket;
 
 namespace RemoteController.ViewModels
 {
     public class MainViewModel : ViewModel
     {
         private IPageViewModel _selected;
-        //public RemoteControllerService Service { get; }
-
-        public WsServer WsServer { get; }//TODO: Remove
-        public ServerConfig ServerConfig { get; }//TODO: Remove
-        public FileSystemConfig FileSystemConfig { get; } //TODO: Remove
 
         /// <summary>
         /// All registered pages.
@@ -32,20 +24,13 @@ namespace RemoteController.ViewModels
             set => Set(ref _selected, value);
         }
 
+        /// <summary>
+        /// Command to select <see cref="IPageViewModel"/>.
+        /// </summary>
         public ICommand SelectCommand { get; }
 
-        public int HexValue { get; } = 0xE105;
-
-        public MainViewModel(
-            WsServer wsServer,
-            ServerConfig serverConfig,
-            FileSystemConfig fileSystemConfig,
-            ILogger logger,
-            IPageViewModel[] pages)
+        public MainViewModel(IPageViewModel[] pages)
         {
-            WsServer = wsServer ?? throw new ArgumentNullException(nameof(wsServer));
-            ServerConfig = serverConfig ?? throw new ArgumentNullException(nameof(serverConfig));
-            FileSystemConfig = fileSystemConfig ?? throw new ArgumentNullException(nameof(fileSystemConfig));
 
             Pages = pages ?? throw new ArgumentNullException(nameof(pages));
             Selected = Pages.FirstOrDefault();
--- RemoteController/RemoteController/ViewModels/Pages/OverviewPageViewModel.cs
+++ RemoteController/RemoteController/ViewModels/Pages/OverviewPageViewModel.cs
@@ -1,12 +1,26 @@
-﻿using System.Windows.Input;
+﻿using System;
+using System.Windows.Input;
+using RemoteController.Configs;
+using RemoteController.WebSocket;
 
 namespace RemoteController.ViewModels.Pages
 {
     public class OverviewPageViewModel : BasePageViewModel
     {
-        public OverviewPageViewModel()
+        public WsServer WsServer { get; }//TODO: Remove
+        public ServerConfig ServerConfig { get; }//TODO: Remove
+        public FileSystemConfig FileSystemConfig { get; } //TODO: Remove
+
+        
+        public OverviewPageViewModel(
+            WsServer wsServer,
+            ServerConfig serverConfig,
+            FileSystemConfig fileSystemConfig)
             : base("Overview")
         {
+            WsServer = wsServer ?? throw new ArgumentNullException(nameof(wsServer));
+            ServerConfig = serverConfig ?? throw new ArgumentNullException(nameof(serverConfig));
+            FileSystemConfig = fileSystemConfig ?? throw new ArgumentNullException(nameof(fileSystemConfig));
         }
 
         /// <summary>
