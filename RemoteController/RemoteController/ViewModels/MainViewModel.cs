aac2bdd5c346f907c19525e8c8083ae897279725
Epsil0neR <epsil0neris@gmail.com>
2020-11-28 19:45:50 +0200

    Created UI for commands. Created generic resource Sizes.

--- RemoteController/RemoteController/ViewModels/MainViewModel.cs
+++ RemoteController/RemoteController/ViewModels/MainViewModel.cs
@@ -39,7 +39,7 @@ namespace RemoteController.ViewModels
         {
             Pages = pages ?? throw new ArgumentNullException(nameof(pages));
             WsServer = wsServer;
-            Selected = Pages.FirstOrDefault(x => x.Name == PageName.Commands);
+            Selected = Pages.FirstOrDefault(x => x.Name == PageName.Paths);
 
             SelectCommand = new RelayCommand<IPageViewModel>(Select);
             StartServerCommand = new RelayCommand(StartServer);
