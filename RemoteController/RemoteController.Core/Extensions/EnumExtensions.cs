c87bafd02550403a13bf1ff94c50344c9814732f
Epsil0neR <epsil0neris@gmail.com>
2020-12-21 01:18:21 +0200

    Created enum to description extension.

---
+++ RemoteController/RemoteController.Core/Extensions/EnumExtensions.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel;
+using System.Reflection;
+
+namespace RemoteController.Extensions
+{
+    public static class EnumExtensions
+    {
+        public static string ToDescription(this Enum @enum)
+        {
+            var rv = @enum.ToString();
+            FieldInfo fieldInfo = @enum.GetType().GetField(rv);
+
+            var attrs = fieldInfo?.GetCustomAttributes(false);
+            if (attrs == null || attrs.Length == 0)
+                return rv;
+
+            var attr = attrs[0] as DescriptionAttribute;
+            return string.IsNullOrWhiteSpace(attr?.Description) 
+                ? rv 
+                : attr.Description;
+        }
+
+    }
+}
