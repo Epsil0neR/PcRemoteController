21e814d3e7a1c843b3ff15b21649edbda847030a
Veniamin Lytkin <epsil0neris@gmail.com>
2023-01-28 19:33:04 +0200

    Use VM instead of wrapping each PageVM to PageView.

---
+++ RemoteController/RemoteController/Views/PageButton.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace RemoteController.Views;
+
+public class PageButton : Button
+{
+    static PageButton()
+    {
+        var t = typeof(PageButton);
+        DefaultStyleKeyProperty.OverrideMetadata(t, new FrameworkPropertyMetadata
+        {
+            DefaultValue = t
+        });
+    }
+}
