d50625bd823b32f910502c18022f7bb6d4b73445
Veniamin Lytkin <epsil0neris@gmail.com>
2023-03-04 16:21:21 +0200

    Create empty WinUI 3 project with templated packaging project.

---
+++ RemoteController/RemoteController.WinUi/RemoteController.WinUi/App.xaml.cs
@@ -0,0 +1,33 @@
+﻿// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.UI.Xaml;
+
+namespace RemoteController;
+
+public partial class App : Application
+{
+    private Window _window;
+    private readonly ServiceProvider _serviceProvider;
+
+    public App()
+    {
+        var sc = new ServiceCollection();
+        ConfigureServices(sc);
+        _serviceProvider = sc.BuildServiceProvider();
+        InitializeComponent();
+    }
+
+    private void ConfigureServices(IServiceCollection serviceCollection)
+    {
+        serviceCollection.AddSingleton<MainWindow>();
+    }
+
+    /// <inheritdoc />
+    protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+    {
+        _window = _serviceProvider.GetRequiredService<MainWindow>();
+        _window.Activate();
+    }
+}
