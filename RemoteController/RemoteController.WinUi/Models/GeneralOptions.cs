b9c101ce4633cddfd45b76b2c99d149841f4218d
Veniamin Lytkin <epsil0neris@gmail.com>
2023-05-21 01:02:53 +0300

    Sample of updating appsettings.json

--- RemoteController/RemoteController.WinUi/Models/GeneralOptions.cs
+++ RemoteController/RemoteController.WinUi/Models/GeneralOptions.cs
@@ -3,4 +3,7 @@
 public class GeneralOptions
 {
     public bool StartupWithOS { get; set; }
+
+    //TODO: Temp stuff.
+    public int ClickCounter { get; set; }
 }
--- RemoteController/RemoteController.WinUi/ViewModels/GenericViewModel.cs
+++ RemoteController/RemoteController.WinUi/ViewModels/GenericViewModel.cs
@@ -1,10 +1,31 @@
-﻿using CommunityToolkit.Mvvm.ComponentModel;
+﻿using System.Windows.Input;
+using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using RemoteController.WinUi.Core.Options;
+using RemoteController.WinUi.Models;
 
 namespace RemoteController.WinUi.ViewModels;
 
 public class GenericViewModel : ObservableRecipient
 {
-    public GenericViewModel()
+    private readonly IWritableOptions<GeneralOptions> _options;
+
+    public GenericViewModel(IWritableOptions<GeneralOptions> options)
     {
+        _options = options;
+
+        IncreaseCounterCommand = new RelayCommand(IncreaseCounter);
+    }
+
+    public int Counter => _options.Value.ClickCounter;
+
+    public ICommand IncreaseCounterCommand { get; }
+
+    private void IncreaseCounter()
+    {
+        _options.Update(o =>
+        {
+            o.ClickCounter++;
+        });
     }
 }
