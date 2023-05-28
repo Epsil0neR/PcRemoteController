74e2bb9c5dd4f690f469d96ad9d1216de42604fd
Veniamin Lytkin <epsil0neris@gmail.com>
2023-05-28 18:45:21 +0300

    Apply changes to existing value as well.

--- RemoteController/RemoteController.WinUi/WritableOptions.cs
+++ RemoteController/RemoteController.WinUi/WritableOptions.cs
@@ -42,10 +42,13 @@ public class WritableOptions<T> : IWritableOptions<T> where T : class, new()
         var physicalPath = fileInfo.PhysicalPath;
 
         var jObject = JsonConvert.DeserializeObject<JObject>(File.ReadAllText(physicalPath));
-        var sectionObject = jObject.TryGetValue(_section, out JToken section) ?
-            JsonConvert.DeserializeObject<T>(section.ToString()) : (Value ?? new T());
+        var sectionObject = jObject.TryGetValue(_section, out JToken section) 
+            ? JsonConvert.DeserializeObject<T>(section.ToString()) 
+            : (Value ?? new T());
 
         applyChanges(sectionObject);
+        if (Value is not null) 
+            applyChanges(Value);
 
         jObject[_section] = JObject.Parse(JsonConvert.SerializeObject(sectionObject));
         File.WriteAllText(physicalPath, JsonConvert.SerializeObject(jObject, Formatting.Indented));
