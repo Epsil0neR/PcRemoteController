1b1a9ae4e5f9974644c612b4ddb1c428c0958c90
Veniamin Lytkin <epsil0neris@gmail.com>
2023-11-17 11:58:15 +0200

    Add project for controls.

---
+++ RemoteController/RemoteController.WinUi.Controls/CustomControl1.cs
@@ -0,0 +1,17 @@
+using Microsoft.UI.Xaml.Controls;
+using Microsoft.UI.Xaml.Markup;
+
+namespace RemoteController.WinUi.Controls;
+
+public class CustomControl1 : Control
+{
+    public CustomControl1()
+    {
+        DefaultStyleKey = typeof(CustomControl1);
+    }
+}
+
+public class Setting : Control
+{
+
+}
