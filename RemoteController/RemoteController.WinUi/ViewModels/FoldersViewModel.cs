da788911b1bfe72db8bad7bdc5cdfba40f959843
Veniamin Lytkin <epsil0neris@gmail.com>
2023-12-09 13:24:25 +0200

    Add card for path with ability to delete path.

--- RemoteController/RemoteController.WinUi/ViewModels/FoldersViewModel.cs
+++ RemoteController/RemoteController.WinUi/ViewModels/FoldersViewModel.cs
@@ -1,14 +1,36 @@
-﻿namespace RemoteController.WinUi.ViewModels;
+﻿using RemoteController.Manipulator.Contexts;
+using RemoteController.WinUi.Core.Options;
+using RemoteController.WinUi.Models;
+
+namespace RemoteController.WinUi.ViewModels;
 
 public partial class FoldersViewModel : ObservableRecipient
 {
-    public FoldersViewModel()
+    public FoldersViewModel(
+        IWritableOptions<FileSystemOptions> fileSystemOptions
+        )
     {
+        FileSystemOptions = fileSystemOptions;
     }
+    public IWritableOptions<FileSystemOptions> FileSystemOptions { get; }
+
+    public FileSystemPaths Paths => FileSystemOptions.Value.Roots;
 
     [RelayCommand]
     void GoToCreate()
     {
 
     }
+
+    [RelayCommand]
+    void EditPath(FileSystemPath path){}
+
+    [RelayCommand(CanExecute = nameof(CanDeletePath))]
+    void DeletePath(FileSystemPath path)
+    {
+        FileSystemOptions.Update(options => options.Roots.Remove(path));
+        OnPropertyChanged(nameof(Paths));
+    }
+
+    bool CanDeletePath(FileSystemPath path) => FileSystemOptions.Value.Roots.Contains(path);
 }
