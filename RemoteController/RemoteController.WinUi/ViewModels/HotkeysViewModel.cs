d2ef19a5022e49a1dd887f17351938ea94002e77
Epsil0neR <epsil0neris@gmail.com>
2024-04-09 00:32:22 +0300

    Improve controls for gestures and simplify hotkeys playground.

--- RemoteController/RemoteController.WinUi/ViewModels/HotkeysViewModel.cs
+++ RemoteController/RemoteController.WinUi/ViewModels/HotkeysViewModel.cs
@@ -1,5 +1,4 @@
-﻿using System.ComponentModel;
-using Epsiloner.WinUi.Gestures;
+﻿using Epsiloner.WinUi.Gestures;
 using Epsiloner.WinUi.Services;
 using Windows.System;
 
@@ -7,45 +6,12 @@ namespace RemoteController.WinUi.ViewModels;
 
 public partial class HotkeysViewModel : ObservableRecipient
 {
-    private readonly int _pageSize = 10;
-    private VirtualKey[] _allKeys;
-
     public IHotkeysService Service { get; }
-
-    [ObservableProperty]
-    private VirtualKey[] _virtualKeys;
-
-    [ObservableProperty]
-    private int _startIndex = 0;
-
-    public int KeysCount => _allKeys.Length;
 
     public HotkeysViewModel(IHotkeysService service)
     {
         Service = service;
         Service.IsPaused = true;
-
-        _allKeys = Enum.GetValues<VirtualKey>();
-        UpdateVirtualKeys();
-
-        PropertyChanged += OnPropertyChanged;
-        OnPropertyChanged(nameof(StartIndex));
-    }
-
-    private void OnPropertyChanged(object? sender, PropertyChangedEventArgs e)
-    {
-        if (e.PropertyName != nameof(StartIndex))
-            return;
-
-        UpdateVirtualKeys();
-    }
-
-    private void UpdateVirtualKeys()
-    {
-        VirtualKeys = _allKeys
-            .Skip(StartIndex)
-            .Take(_pageSize)
-            .ToArray();
     }
 
     [RelayCommand]
@@ -54,24 +20,10 @@ public partial class HotkeysViewModel : ObservableRecipient
         Service.ReattachHooks();
     }
 
-    [RelayCommand]
-    void PrevPage()
-    {
-        StartIndex = Math.Max(0, StartIndex - _pageSize);
-        UpdateVirtualKeys();
-    }
-
-    [RelayCommand]
-    void NextPage()
-    {
-        StartIndex += _pageSize;
-        UpdateVirtualKeys();
-    }
-
     public MultiKeyGesture TestGestures { get; } = new(new Gesture[]
     {
         new(VirtualKey.A, VirtualKeyModifiers.Control | VirtualKeyModifiers.Menu | VirtualKeyModifiers.Shift | VirtualKeyModifiers.Windows),
-        new(VirtualKey.B),
+        new(VirtualKey.B, VirtualKeyModifiers.Shift),
         new(VirtualKey.Left),
         new(VirtualKey.Up),
         new(VirtualKey.Right),
