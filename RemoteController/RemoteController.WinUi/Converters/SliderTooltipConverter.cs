e8f69910bcd282cead50b7da263205a109ad9b01
Epsil0neR <epsil0neris@gmail.com>
2024-05-11 15:50:09 +0300

    Add converter for volume sliders.

---
+++ RemoteController/RemoteController.WinUi/Converters/SliderTooltipConverter.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.UI.Xaml.Data;
+
+namespace RemoteController.WinUi.Converters;
+
+public class SliderTooltipConverter : IValueConverter
+{
+    public string LeftPart { get; set; }
+
+    public string RightPart { get; set; }
+
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        return $"{LeftPart}{value}{RightPart}";
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
