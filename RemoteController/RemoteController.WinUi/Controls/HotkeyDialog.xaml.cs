32f63b90e8fd13b31725726270f8766bcaa47a69
Epsil0neR <epsil0neris@gmail.com>
2024-10-05 03:27:27 +0300

    Opened dialog suspends hotkeys while opened.

--- RemoteController/RemoteController.WinUi/Controls/HotkeyDialog.xaml.cs
+++ RemoteController/RemoteController.WinUi/Controls/HotkeyDialog.xaml.cs
@@ -4,13 +4,39 @@ namespace RemoteController.WinUi.Controls;
 
 public sealed partial class HotkeyDialog
 {
+    public static readonly DependencyProperty ViewModelProperty = DependencyProperty.Register(
+        nameof(ViewModel), typeof(HotkeyGestureEditorViewModel), typeof(HotkeyDialog), new(null, ViewModelPropertyChangedCallback));
+    
+    public HotkeyGestureEditorViewModel? ViewModel
+    {
+        get => (HotkeyGestureEditorViewModel)GetValue(ViewModelProperty);
+        set => SetValue(ViewModelProperty, value);
+    }
+
     public HotkeyDialog()
     {
         InitializeComponent();
+
+        // Pause hotkeys while dialog is opened:
+        Opened += OnOpened;
+        Closed += OnClosed;
     }
 
-    public static readonly DependencyProperty ViewModelProperty = DependencyProperty.Register(
-        nameof(ViewModel), typeof(HotkeyGestureEditorViewModel), typeof(HotkeyDialog), new(null, ViewModelPropertyChangedCallback));
+    private void OnOpened(ContentDialog sender, ContentDialogOpenedEventArgs args)
+    {
+        if (ViewModel is null)
+            return;
+
+        ViewModel.HotkeysService.IsPaused = true;
+    }
+
+    private void OnClosed(ContentDialog sender, ContentDialogClosedEventArgs args)
+    {
+        if (ViewModel is null)
+            return;
+
+        ViewModel.HotkeysService.IsPaused = false;
+    }
 
     private static void ViewModelPropertyChangedCallback(DependencyObject d, DependencyPropertyChangedEventArgs e)
     {
@@ -23,12 +49,5 @@ public sealed partial class HotkeyDialog
         {
             value.EditGestureDialog = (HotkeyDialog)d;
         }
-
-    }
-
-    public HotkeyGestureEditorViewModel ViewModel
-    {
-        get => (HotkeyGestureEditorViewModel)GetValue(ViewModelProperty);
-        set => SetValue(ViewModelProperty, value);
     }
 }
