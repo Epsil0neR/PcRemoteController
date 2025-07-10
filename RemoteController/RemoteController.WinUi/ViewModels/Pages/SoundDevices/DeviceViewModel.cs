6df089bc09c662924f705993ffca70bca36e640e
Epsil0neR <epsil0neris@gmail.com>
2025-07-10 16:34:38 +0300

    Add volume lock for sound input/output devices.

--- RemoteController/RemoteController.WinUi/ViewModels/Pages/SoundDevices/DeviceViewModel.cs
+++ RemoteController/RemoteController.WinUi/ViewModels/Pages/SoundDevices/DeviceViewModel.cs
@@ -1,10 +1,14 @@
 using CommunityToolkit.Mvvm.Messaging;
+using Epsiloner.Cooldowns;
+using Newtonsoft.Json.Linq;
 using RemoteController.WinUi.Messages;
 
 namespace RemoteController.WinUi.ViewModels.Pages.SoundDevices;
 
 public partial class DeviceViewModel : ObservableObject
 {
+    private readonly EventCooldown _lockedVolumeCooldown;
+
     public IMessenger Messenger { get; set; }
 
     public required string Name { get; init; }
@@ -27,11 +31,19 @@ public partial class DeviceViewModel : ObservableObject
     /// Device volume in 0..100 range.
     /// </summary>
     [ObservableProperty]
-    public int _volume;
+    private int _volume;
+
+    /// <summary>
+    /// Device volume in 0..100 range to be always forced to be set.
+    /// </summary>
+    [ObservableProperty]
+    private int? _lockedVolume;
 
     public DeviceViewModel(IMessenger messenger)
     {
         Messenger = messenger;
+        var delay = TimeSpan.FromMicroseconds(200);
+        _lockedVolumeCooldown = new EventCooldown(delay, ApplyLockedVolume, delay);
     }
 
     partial void OnIsSelectedChanged(bool value)
@@ -44,13 +56,52 @@ public partial class DeviceViewModel : ObservableObject
         Messenger.Send(new ChangeVolumeForDeviceRequest()
         {
             Device = this,
-            Volume = value
+            Volume = LockedVolume ?? value
         });
+
+        if (LockedVolume.HasValue && LockedVolume != Volume)
+            _lockedVolumeCooldown.Accumulate();
     }
 
-    [RelayCommand()]
+    [RelayCommand]
     private void MakeSystemDefault()
     {
         Messenger.Send(new SystemDefaultSoundDeviceRequest() { Device = this });
+    }
+
+    [RelayCommand]
+    private void ToggleLockVolume()
+    {
+        LockedVolume = LockedVolume is null
+            ? Volume
+            : null;
+    }
+
+    private void ApplyLockedVolume()
+    {
+        Task.Run(ApplyLockedVolumeAsync)
+            .GetAwaiter()
+            .GetResult();
+    }
+
+    private async Task ApplyLockedVolumeAsync()
+    {
+        if (LockedVolume is null)
+            return;
+        if (LockedVolume == Volume)
+            return;
+
+        var vol = LockedVolume.Value;
+
+        do
+        {
+            Messenger.Send(new ChangeVolumeForDeviceRequest()
+            {
+                Device = this,
+                Volume = vol
+            });
+
+            await Task.Delay(200);
+        } while (Volume != vol);
     }
 }
