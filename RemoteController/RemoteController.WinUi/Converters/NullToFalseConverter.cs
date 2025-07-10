ca1c2f4aee4c64333d76f12755a77fd8fab81e93
Epsil0neR <epsil0neris@gmail.com>
2025-07-10 16:34:18 +0300

    Add more converters.

---
+++ RemoteController/RemoteController.WinUi/Converters/NullToFalseConverter.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.UI.Xaml.Data;
+
+namespace RemoteController.WinUi.Converters;
+
+public class NullToFalseConverter : IValueConverter
+{
+    public object Convert(object value, Type targetType, object parameter, string language)
+    {
+        return !ReferenceEquals(value, null);
+    }
+
+    public object ConvertBack(object value, Type targetType, object parameter, string language)
+    {
+        throw new NotImplementedException();
+    }
+}
